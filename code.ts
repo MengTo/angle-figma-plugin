25268cb794c17b8b287a65b278dddb3e1a8c54d2
Natalie Smith <thatgalnatalie@gmail.com>
2019-11-15 06:38:46 -0800

    added pixel density + fixed infinite loading

--- code.ts
+++ code.ts
@@ -1,14 +1,8 @@
+import { TIMEOUT } from 'dns';
+
 figma.showUI(__html__, { width: 790, height: 475 });
 
 // figma.currentPage.selection[0].parent.exportAsync().then(response => console.log(response));
-
-/*
-FIXME BUGS THAT PREVENT IT FROM RUNNING 
-1. IF THE SCREEN IS A RECTANGLE NODE 
-2. IF THE SELECTED ARTBOARD HAS TOO MANY PONITS 
-3. NEEDS A LOADING BAR  
-4. GET RID OF THAT APPLY MOCKUP BUTTON ERROR 
-*/
 
 // All Variables
 const currentUserSelection = figma.currentPage.selection[0];
@@ -18,9 +12,9 @@ const testerNode = allFigmaNodes[2];
 // NOTE Loop Over All The Node Names And Send It To The UI
 let nodesNames = [];
 figma.currentPage.children.map(function(node) {
-  nodesNames.push(node.name);
+	nodesNames.push(node.name);
 });
-figma.ui.postMessage({ type: "allNodeNames", nodeNames: nodesNames });
+figma.ui.postMessage({ type: 'allNodeNames', nodeNames: nodesNames });
 
 /*
 NOTE Steps For The Angle Fill (Perspective Transform)
@@ -31,151 +25,148 @@ NOTE Steps For The Angle Fill (Perspective Transform)
 */
 
 function clone(val) {
-  return JSON.parse(JSON.stringify(val));
+	return JSON.parse(JSON.stringify(val));
 }
 
 // Will Loop Over All The Nodes And Return The Selected Node
 function findSelectedNode(selectedNodeName) {
-  var result;
-  allFigmaNodes.filter(node => {
-    if (node.name === selectedNodeName) {
-      result = node;
-    }
-  });
-  return result;
+	var result;
+	allFigmaNodes.filter(node => {
+		if (node.name === selectedNodeName) {
+			result = node;
+		}
+	});
+	return result;
 }
 
 // Returns The Bytes Of The Passed In Node
 async function invertImages(node) {
-  const newFills = [];
-  for (const paint of node.fills) {
-    // Get the (encoded) bytes for this image.
-    if (paint.type === "IMAGE") {
-      const image = figma.getImageByHash(paint.imageHash);
-      const bytes = await image.getBytesAsync();
+	const newFills = [];
+	for (const paint of node.fills) {
+		// Get the (encoded) bytes for this image.
+		if (paint.type === 'IMAGE') {
+			const image = figma.getImageByHash(paint.imageHash);
+			const bytes = await image.getBytesAsync();
 
-      return bytes;
-    }
-  }
+			return bytes;
+		}
+	}
 }
 
 // Returns The Byte Of The Node If It Is A Group || Frame
 async function invertNode(node) {
-  // figma.currentPage.selection[0].parent.exportAsync().then(response => console.log(response));
-  const unit8 = await node.exportAsync();
-  return unit8;
+	// figma.currentPage.selection[0].parent.exportAsync().then(response => console.log(response));
+	const unit8 = await node.exportAsync();
+	return unit8;
 }
 
 function angleFill(array, node) {
-  const newFills = [];
-  for (const paint of node.fills) {
-    if (paint.type === "IMAGE") {
-      const newPaint = JSON.parse(JSON.stringify(paint));
-      newPaint.imageHash = figma.createImage(array).hash;
-      newFills.push(newPaint);
-    }
-  }
-  return newFills;
+	const newFills = [];
+	for (const paint of node.fills) {
+		if (paint.type === 'IMAGE') {
+			const newPaint = JSON.parse(JSON.stringify(paint));
+			newPaint.imageHash = figma.createImage(array).hash;
+			newFills.push(newPaint);
+		}
+	}
+	return newFills;
 }
 
 // Listen For All postMessages Coming Back From The UI
-figma.ui.on("message", uiResponse => {
-  try {
-    if (uiResponse.type === "convertSelectedArtboard") {
-      const selectedNode = findSelectedNode(uiResponse.selectedArtboard);
+figma.ui.on('message', uiResponse => {
+	try {
+		if (uiResponse.type === 'convertSelectedArtboard') {
+			if (uiResponse.selectedArtboard.length !== 0) {
+				const selectedNode = findSelectedNode(uiResponse.selectedArtboard);
+				console.log('not empty');
+				const coordinates = {};
+				if (currentUserSelection.type === 'VECTOR') {
+					coordinates.topLeftX = currentUserSelection.vectorNetwork.vertices[0].x;
+					coordinates.topLeftY = currentUserSelection.vectorNetwork.vertices[0].y;
 
-      const coordinates = {};
-      if (currentUserSelection.type === "VECTOR") {
-        coordinates.topLeftX = currentUserSelection.vectorNetwork.vertices[0].x;
-        coordinates.topLeftY = currentUserSelection.vectorNetwork.vertices[0].y;
+					// // TOP RIGHT
+					coordinates.topRightX = currentUserSelection.vectorNetwork.vertices[1].x;
+					coordinates.topRightY = currentUserSelection.vectorNetwork.vertices[1].y;
 
-        // // TOP RIGHT
-        coordinates.topRightX =
-          currentUserSelection.vectorNetwork.vertices[1].x;
-        coordinates.topRightY =
-          currentUserSelection.vectorNetwork.vertices[1].y;
+					// // BOTTOM LEFT
+					coordinates.bottomLeftX = currentUserSelection.vectorNetwork.vertices[2].x;
+					coordinates.bottomLeftY = currentUserSelection.vectorNetwork.vertices[2].y;
 
-        // // BOTTOM LEFT
-        coordinates.bottomLeftX =
-          currentUserSelection.vectorNetwork.vertices[2].x;
-        coordinates.bottomLeftY =
-          currentUserSelection.vectorNetwork.vertices[2].y;
+					// // BOTTOM RIGHT
+					coordinates.bottomRightX = currentUserSelection.vectorNetwork.vertices[3].x;
+					coordinates.bottomRightY = currentUserSelection.vectorNetwork.vertices[3].y;
 
-        // // BOTTOM RIGHT
-        coordinates.bottomRightX =
-          currentUserSelection.vectorNetwork.vertices[3].x;
-        coordinates.bottomRightY =
-          currentUserSelection.vectorNetwork.vertices[3].y;
+					invertImages(selectedNode).then(arr => {
+						figma.ui.postMessage({
+							type: 'networkRequest',
+							uint8Array: arr,
+							ponits: coordinates,
+							selectedPixelDensity: uiResponse.selectedPixelDensity,
+							width: currentUserSelection.width,
+							height: currentUserSelection.height
+						});
+					});
+				}
 
-        invertImages(selectedNode).then(arr => {
-          figma.ui.postMessage({
-            type: "networkRequest",
-            uint8Array: arr,
-            ponits: coordinates,
-            width: currentUserSelection.width,
-            height: currentUserSelection.height
-          });
-        });
-      }
+				if (currentUserSelection.type === 'RECTANGLE') {
+					figma.notify(
+						`Your current selected screen is a ${currentUserSelection.type} node. Please choose a Vector node`
+					);
+				}
 
-      if (currentUserSelection.type === "RECTANGLE") {
-        figma.notify(
-          `Your current selected screen is a ${currentUserSelection.type} node. Please choose a Vector node`
-        );
-      }
+				const selectedOrientation = uiResponse.selectedOrientation;
+				const selectedQuality = uiResponse.selectedQuality;
+				const selectedPixelDensity = uiResponse.selectedPixelDensity;
 
-      const selectedOrientation = uiResponse.selectedOrientation;
-      const selectedQuality = uiResponse.selectedQuality;
-      const selectedPixelDensity = uiResponse.selectedPixelDensity;
+				// User Selection Of Artboard
+				if (selectedNode.type === 'FRAME' || selectedNode.type === 'GROUP') {
+					invertNode(selectedNode).then(response => {
+						figma.ui.postMessage({
+							selectedOrientation: selectedOrientation,
+							selectedQuality: selectedQuality,
+							selectedPixelDensity: selectedPixelDensity,
+							type: 'networkRequest',
+							uint8Array: response,
+							ponits: coordinates,
+							width: currentUserSelection.width,
+							height: currentUserSelection.height
+						});
+					});
 
-      // User Selection Of Artboard
-      if (selectedNode.type === "FRAME" || selectedNode.type === "GROUP") {
-        invertNode(selectedNode).then(response => {
-          figma.ui.postMessage({
-            selectedOrientation: selectedOrientation,
-            selectedQuality: selectedQuality,
-            selectedPixelDensity: selectedPixelDensity,
-            type: "networkRequest",
-            uint8Array: response,
-            ponits: coordinates,
-            width: currentUserSelection.width,
-            height: currentUserSelection.height
-          });
-        });
+					// base image if the selected artboard is a frame or group
+					for (const selectedNode of figma.currentPage.selection) {
+						const fills = clone(selectedNode.fills || []);
+						fills.push({
+							type: 'IMAGE',
+							scaleMode: 'FIT',
+							imageHash: figma.createImage(new Uint8Array()).hash
+						});
+						currentUserSelection.fills = fills;
+					}
+				}
 
-        // base image if the selected artboard is a frame or group
-        for (const selectedNode of figma.currentPage.selection) {
-          const fills = clone(selectedNode.fills || []);
-          fills.push({
-            type: "IMAGE",
-            scaleMode: "FIT",
-            imageHash: figma.createImage(new Uint8Array()).hash
-          });
-          currentUserSelection.fills = fills;
-        }
-      }
+				// base image if the selected artboard is an image
+				const cloneOfScreen = clone(figma.currentPage.selection[0].fills);
+				const selectedImage = selectedNode.fills;
+				cloneOfScreen[0] = selectedImage[0];
+				figma.currentPage.selection[0].fills = cloneOfScreen;
+			} else if (uiResponse.selectedArtboard.length === 0) {
+				figma.closePlugin();
+				figma.notify('Please choose an artboard');
+			}
+		} else if (uiResponse.type === 'networkResponse') {
+			const cloneOfScreen = clone(figma.currentPage.selection[0].fills);
+			const selectedImage = angleFill(uiResponse.response, currentUserSelection);
+			cloneOfScreen[0] = selectedImage[0];
+			figma.currentPage.selection[0].fills = cloneOfScreen;
 
-      // base image if the selected artboard is an image
-      const cloneOfScreen = clone(figma.currentPage.selection[0].fills);
-      const selectedImage = selectedNode.fills;
-      cloneOfScreen[0] = selectedImage[0];
-      figma.currentPage.selection[0].fills = cloneOfScreen;
-    } else if (uiResponse.type === "networkResponse") {
-      const cloneOfScreen = clone(figma.currentPage.selection[0].fills);
-      const selectedImage = angleFill(
-        uiResponse.response,
-        currentUserSelection
-      );
-      cloneOfScreen[0] = selectedImage[0];
-      figma.currentPage.selection[0].fills = cloneOfScreen;
-
-      figma.closePlugin();
-    } else if (uiResponse.type === "cancel-modal") {
-      figma.closePlugin();
-    } else if (uiResponse.type === "netWorkError") {
-      figma.notify(uiResponse.message);
-    }
-  } catch (error) {
-    console.log("loading...");
-  }
+			figma.closePlugin();
+		} else if (uiResponse.type === 'cancel-modal') {
+			figma.closePlugin();
+		} else if (uiResponse.type === 'netWorkError') {
+			figma.notify(uiResponse.message);
+		}
+	} catch (error) {
+		console.log('loading...');
+	}
 });
