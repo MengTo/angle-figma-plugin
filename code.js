3278720728740f31fb16ab1b342af71026bb24a8
Natalie Smith <thatgalnatalie@gmail.com>
2019-11-29 07:49:20 -0800

    removes extra fills

--- code.js
+++ code.js
@@ -7,43 +7,49 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-if (figma.command === "applyMockup") {
+function clone(val) {
+    return JSON.parse(JSON.stringify(val));
+}
+if (figma.command === 'applyMockup') {
     figma.showUI(__html__, { width: 790, height: 500 });
 }
 if (figma.currentPage.selection.length === 0) {
     figma.closePlugin();
-    figma.notify("Please select a Vector.");
+    figma.notify('Please select a Vector.');
+}
+// removes extra fills
+if (figma.currentPage.selection[0].fills.length > 1) {
+    const copyOfFills = clone(figma.currentPage.selection[0].fills);
+    copyOfFills.splice(1);
+    figma.currentPage.selection[0].fills = copyOfFills;
 }
 try {
-    function clone(val) {
-        return JSON.parse(JSON.stringify(val));
-    }
-    if (figma.currentPage.selection[0].type === "FRAME" ||
-        figma.currentPage.selection[0].type === "GROUP") {
+    if (figma.currentPage.selection[0].type === 'FRAME' ||
+        figma.currentPage.selection[0].type === 'GROUP') {
         figma.closePlugin();
-        figma.notify("Please select a Vector.");
+        figma.notify('Please select a Vector.');
     }
     if (figma.currentPage.selection[0].fills === undefined ||
         figma.currentPage.selection[0].fills.length === 0) {
         const fills = Array.from(figma.currentPage.selection[0].fills);
         fills.push({
-            type: "IMAGE",
+            type: 'IMAGE',
             visible: true,
             opacity: 1,
-            scaleMode: "FILL",
-            imageHash: "efe98099a0aa97c1aa64e286bc82e633cc9aed22"
+            scaleMode: 'FILL',
+            imageHash: 'efe98099a0aa97c1aa64e286bc82e633cc9aed22'
         });
         figma.currentPage.selection[0].fills = fills;
     }
     var convertedNode = {};
     // NOTE Convert Rectangle Node to Vector Node
-    if (figma.currentPage.selection[0].type === "RECTANGLE") {
+    if (figma.currentPage.selection[0].type === 'RECTANGLE') {
         // NOTE faltten the currentSelection to convert to vector -> this will lose the currentSelection value
         convertedNode = figma.flatten(figma.currentPage.selection);
     }
     if (convertedNode.type === undefined) {
         // The selection is not a rectangle
-        if (figma.currentPage.selection[0].type !== "VECTOR") {
+        if (figma.currentPage.selection[0].type !== 'VECTOR') {
             figma.closePlugin();
             figma.notify(`Your current selected screen is a ${figma.currentPage.selection[0].type} node. Please choose a Vector node instead.`);
         }
@@ -57,7 +63,7 @@ try {
     // NOTE Loop Over All The Node Names And Send It To The UI
     let nodesNames = [];
     const getRootFrame = node => {
-        if (node.parent && node.parent.type === "FRAME") {
+        if (node.parent && node.parent.type === 'FRAME') {
             return node.parent.name;
         }
         else {
@@ -81,18 +87,18 @@ try {
         // }
         // NOTE for now
         // nodesNames.push(node.name);
-        if (node.type === "FRAME") {
+        if (node.type === 'FRAME') {
             nodesNames.push(node.name);
         }
     });
-    figma.ui.postMessage({ type: "allNodeNames", nodeNames: nodesNames });
+    figma.ui.postMessage({ type: 'allNodeNames', nodeNames: nodesNames });
     /*
-  NOTE Steps For The Angle Fill (Perspective Transform)
-  1. Recieve The Selected Node From The User Via The UI and Grab The Unit 8 Array From That
-  2. Convert The Unit 8 Array Into a Base64 String
-  3. Grab The 8 Ponits, Width, and Height, and Base64 String Of The Selected Node And Send it Back To The UI to Make An API Call
-  4. Recieve The Modifyed Unit8 Array From The API And Replace That As the Unit8 Array in The Fill Of The Selected Node
-  */
+NOTE Steps For The Angle Fill (Perspective Transform)
+1. Recieve The Selected Node From The User Via The UI and Grab The Unit 8 Array From That
+2. Convert The Unit 8 Array Into a Base64 String
+3. Grab The 8 Ponits, Width, and Height, and Base64 String Of The Selected Node And Send it Back To The UI to Make An API Call
+4. Recieve The Modifyed Unit8 Array From The API And Replace That As the Unit8 Array in The Fill Of The Selected Node
+*/
     // Will Loop Over All The Nodes And Return The Selected Node
     function findSelectedNode(selectedNodeName) {
         var result;
@@ -108,7 +114,7 @@ try {
         return __awaiter(this, void 0, void 0, function* () {
             for (const paint of node.fills) {
                 // Get the (encoded) bytes for this image.
-                if (paint.type === "IMAGE") {
+                if (paint.type === 'IMAGE') {
                     const image = figma.getImageByHash(paint.imageHash);
                     const bytes = yield image.getBytesAsync();
                     return bytes;
@@ -121,8 +127,8 @@ try {
         return __awaiter(this, void 0, void 0, function* () {
             // figma.currentPage.selection[0].parent.exportAsync().then(response => console.log(response));
             const unit8 = yield node.exportAsync({
-                format: "PNG",
-                constraint: { type: "SCALE", value: 3 }
+                format: 'PNG',
+                constraint: { type: 'SCALE', value: 3 }
             });
             return unit8;
         });
@@ -130,7 +136,7 @@ try {
     function angleFill(array, node) {
         const newFills = [];
         for (const paint of node.fills) {
-            if (paint.type === "IMAGE") {
+            if (paint.type === 'IMAGE') {
                 const newPaint = JSON.parse(JSON.stringify(paint));
                 newPaint.imageHash = figma.createImage(array).hash;
                 newFills.push(newPaint);
@@ -140,13 +146,13 @@ try {
     }
     function angleArtboard(node, artboard, orientation, pixel, quality, coordinates, nodeWidth, nodeHeight) {
         // User Selection Of Artboard
-        if (artboard.type === "FRAME" || artboard.type === "GROUP") {
+        if (artboard.type === 'FRAME' || artboard.type === 'GROUP') {
             invertNode(artboard).then(response => {
                 figma.ui.postMessage({
                     selectedOrientation: orientation,
                     selectedQuality: quality,
                     selectedPixelDensity: pixel,
-                    type: "networkRequest",
+                    type: 'networkRequest',
                     uint8Array: response,
                     ponits: coordinates,
                     width: node.width,
@@ -157,11 +163,11 @@ try {
                 if (node.fills) {
                     const fills = Array.from(node.fills);
                     fills.push({
-                        type: "IMAGE",
+                        type: 'IMAGE',
                         visible: true,
                         opacity: 1,
-                        scaleMode: "FILL",
-                        imageHash: "efe98099a0aa97c1aa64e286bc82e633cc9aed22"
+                        scaleMode: 'FILL',
+                        imageHash: 'efe98099a0aa97c1aa64e286bc82e633cc9aed22'
                     });
                     node.fills = fills;
                 }
@@ -170,39 +176,31 @@ try {
         }
     }
     // Listen For All postMessages Coming Back From The UI
-    figma.ui.on("message", uiResponse => {
+    figma.ui.on('message', uiResponse => {
         try {
-            if (uiResponse.type === "convertSelectedArtboard") {
+            if (uiResponse.type === 'convertSelectedArtboard') {
                 if (uiResponse.selectedArtboard.length !== 0) {
                     const selectedNode = findSelectedNode(uiResponse.selectedArtboard);
                     const coordinates = {};
                     // console.log('currentSelection', currentUserSelection);
                     // console.log('convertedNode', convertedNode);
                     if (currentUserSelection !== undefined &&
-                        currentUserSelection.type === "VECTOR") {
-                        console.log("not a rectangle", currentUserSelection);
-                        coordinates.topLeftX =
-                            currentUserSelection.vectorNetwork.vertices[0].x;
-                        coordinates.topLeftY =
-                            currentUserSelection.vectorNetwork.vertices[0].y;
+                        currentUserSelection.type === 'VECTOR') {
+                        console.log('not a rectangle', currentUserSelection);
+                        coordinates.topLeftX = currentUserSelection.vectorNetwork.vertices[0].x;
+                        coordinates.topLeftY = currentUserSelection.vectorNetwork.vertices[0].y;
                         // // TOP RIGHT
-                        coordinates.topRightX =
-                            currentUserSelection.vectorNetwork.vertices[1].x;
-                        coordinates.topRightY =
-                            currentUserSelection.vectorNetwork.vertices[1].y;
+                        coordinates.topRightX = currentUserSelection.vectorNetwork.vertices[1].x;
+                        coordinates.topRightY = currentUserSelection.vectorNetwork.vertices[1].y;
                         // // BOTTOM LEFT
-                        coordinates.bottomLeftX =
-                            currentUserSelection.vectorNetwork.vertices[2].x;
-                        coordinates.bottomLeftY =
-                            currentUserSelection.vectorNetwork.vertices[2].y;
+                        coordinates.bottomLeftX = currentUserSelection.vectorNetwork.vertices[2].x;
+                        coordinates.bottomLeftY = currentUserSelection.vectorNetwork.vertices[2].y;
                         // // BOTTOM RIGHT
-                        coordinates.bottomRightX =
-                            currentUserSelection.vectorNetwork.vertices[3].x;
-                        coordinates.bottomRightY =
-                            currentUserSelection.vectorNetwork.vertices[3].y;
+                        coordinates.bottomRightX = currentUserSelection.vectorNetwork.vertices[3].x;
+                        coordinates.bottomRightY = currentUserSelection.vectorNetwork.vertices[3].y;
                         invertImages(selectedNode).then(arr => {
                             figma.ui.postMessage({
-                                type: "networkRequest",
+                                type: 'networkRequest',
                                 uint8Array: arr,
                                 ponits: coordinates,
                                 selectedPixelDensity: uiResponse.selectedPixelDensity,
@@ -213,9 +211,8 @@ try {
                         });
                         angleArtboard(currentUserSelection, selectedNode, uiResponse.selectedOrientation, uiResponse.selectedPixelDensity, uiResponse.selectedQuality, coordinates, currentUserSelection.width, currentUserSelection.height);
                     }
-                    if (currentUserSelection === undefined &&
-                        convertedNode.type === "VECTOR") {
-                        console.log("converting rectangle to vector...");
+                    if (currentUserSelection === undefined && convertedNode.type === 'VECTOR') {
+                        console.log('converting rectangle to vector...');
                         coordinates.topLeftX = convertedNode.vectorNetwork.vertices[0].x;
                         coordinates.topLeftY = convertedNode.vectorNetwork.vertices[0].y;
                         // // TOP RIGHT
@@ -225,14 +222,12 @@ try {
                         coordinates.bottomLeftX = convertedNode.vectorNetwork.vertices[2].x;
                         coordinates.bottomLeftY = convertedNode.vectorNetwork.vertices[2].y;
                         // // BOTTOM RIGHT
-                        coordinates.bottomRightX =
-                            convertedNode.vectorNetwork.vertices[3].x;
-                        coordinates.bottomRightY =
-                            convertedNode.vectorNetwork.vertices[3].y;
+                        coordinates.bottomRightX = convertedNode.vectorNetwork.vertices[3].x;
+                        coordinates.bottomRightY = convertedNode.vectorNetwork.vertices[3].y;
                         console.log(coordinates);
                         invertImages(selectedNode).then(arr => {
                             figma.ui.postMessage({
-                                type: "networkRequest",
+                                type: 'networkRequest',
                                 uint8Array: arr,
                                 ponits: coordinates,
                                 selectedPixelDensity: uiResponse.selectedPixelDensity,
@@ -254,12 +249,12 @@ try {
                 }
                 else if (uiResponse.selectedArtboard.length === 0) {
                     figma.closePlugin();
-                    figma.notify("Please choose an artboard");
+                    figma.notify('Please choose an artboard');
                 }
             }
-            else if (uiResponse.type === "networkResponse") {
+            else if (uiResponse.type === 'networkResponse') {
                 if (figma.currentPage.selection[0] === undefined &&
-                    convertedNode.type === "VECTOR") {
+                    convertedNode.type === 'VECTOR') {
                     const cloneOfScreen = clone(convertedNode.fills);
                     const fills = clone(convertedNode.fills);
                     const selectedImage = angleFill(uiResponse.response, convertedNode);
@@ -283,20 +278,20 @@ try {
                     figma.closePlugin();
                 }
             }
-            else if (uiResponse.type === "cancel-modal") {
+            else if (uiResponse.type === 'cancel-modal') {
                 figma.closePlugin();
             }
-            else if (uiResponse.type === "netWorkError") {
+            else if (uiResponse.type === 'netWorkError') {
                 figma.notify(uiResponse.message);
             }
-            else if (uiResponse.type === "cloudinaryError") {
+            else if (uiResponse.type === 'cloudinaryError') {
                 figma.closePlugin();
                 figma.notify(uiResponse.message);
             }
             console.log(uiResponse);
         }
         catch (error) {
-            console.log("loading...");
+            console.log('loading...');
         }
     });
 }
